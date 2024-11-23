caa36a7d9b7d5999abc34557e4dc3cfc3def87a2
michael <michael>
2024-11-23 11:01:17 +0700

    update

--- SD19314/Demo-Vue/Buoi5/src/main.js
+++ SD19314/Demo-Vue/Buoi5/src/main.js
@@ -10,3 +10,10 @@ const app = createApp(App)
 // app.use(router)
 app.component("Global", GlobalComponent)
 app.mount('#app')
+
+// Ôn tập table, v-for, v-if
+// Tạo component và sử dụng trong App.vue
+// Tạo Global component và sử dụng
+// Truyền giá trị từ cha qua con bằng props (defineProps)
+// Gọi event (emit event) tới component cha với defineEmits
+// Data binding 2 chiều qua form, với form nằm trong 1 component khác, bằng v-model
